00030112a0112597668ad42a2761e0cba4438225
OreoProMax <OuHY11228@gmail.com>
2026-04-11 19:07:47 +0800

    初始化

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# Vue 3 + TypeScript + Vite
+# paipai-watcher
 
-This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+[![GLWTPL](https://img.shields.io/badge/GLWT-Public_License-red.svg)](https://github.com/me-shaon/GLWTPL)
 
-## Recommended IDE Setup
+基于 [vite-plugin-monkey](https://github.com/lisonge/vite-plugin-monkey) 开发。
 
-- [VS Code](https://code.visualstudio.com/) + [Vue - Official](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+## License
 
-## Type Support For `.vue` Imports in TS
-
-TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [Vue - Official](https://marketplace.visualstudio.com/items?itemName=Vue.volar) to make the TypeScript language service aware of `.vue` types.
+This project is licensed under [GLWTPL](./LICENSE)
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,31 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import monkey, { cdn } from 'vite-plugin-monkey';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import monkey, { cdn } from "vite-plugin-monkey";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     monkey({
-      entry: 'src/main.ts',
+      entry: "src/main.ts",
       userscript: {
-        icon: 'https://vitejs.dev/logo.svg',
-        namespace: 'npm/vite-plugin-monkey',
-        match: ['https://www.google.com/'],
+        name: "paipai-watcher",
+        version: "0.1",
+        icon: "https://ydcx.360buyimg.com/favicon.ico",
+        match: ["https://paipai.m.jd.com/ppinspect/*"],
+        author: "OreoProMax",
+        license: "GLWTPL",
+        namespace: "https://github.com/OreoProMax/paipai-watcher",
+        updateURL:
+          "https://raw.githubusercontent.com/OreoProMax/paipai-watcher/master/dist/paipai-watcher.min.user.js",
+        downloadURL:
+          "https://raw.githubusercontent.com/OreoProMax/paipai-watcher/master/dist/paipai-watcher.min.user.js",
+        supportURL: "https://github.com/OreoProMax/paipai-watcher/issues",
       },
       build: {
         externalGlobals: {
-          vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
+          vue: cdn.jsdelivr("Vue", "dist/vue.global.prod.js"),
+          "element-plus": cdn.jsdelivr("ElementPlus", "dist/index.full.min.js"),
         },
       },
     }),
